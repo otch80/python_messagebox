f0e768f3a06c4eebfcefd43aca57f9d8d8b0c81d
otch80 <minkk1224@gmail.com>
2021-08-09 14:35:01 +0900

    WinAPI를 활용한 MSG Box

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import ctypes
 
 def messageBox(title, text, style):
-	return ctypes.windll.user32.MessageBoxW(None, text, title, style)
+	return ctypes.windll.user32.MessageBoxW(0, text, title, style)
 
 if __name__=="__main__":
     messageBox("Error","에러발생",0)
